e9eee4c41dae93f7f6a44340504cbf5f01d41afb
FacuDun <facundotomasdun10@gmail.com>
2025-05-07 14:02:40 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*", // Permitir cualquier origen (en producción, usa tu dominio)
+        origin: "https://facudun.github.io/Front-pregunta-respuesta/", // Permitir cualquier origen (en producción, usa tu dominio)
     },
 });
 
